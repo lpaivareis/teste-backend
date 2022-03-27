2ee9ed81f09f00bd5dedf0068c7f9c1752dbca13
Lucas Paiva <lucasfelipedepaiva2@gmail.com>
2022-03-27 15:42:07 -0300

    add validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
     validates :name,  length: {minimum:4}
     validates :active, :birthday, :email, :mobile, :advertising, presence: true
+    validates :document, length: 11..14, presence: true, format: { with: /\A^[0-9]*$\z/i, message: "Por favor preencha o campo CPF/CNPJ apenas com números." }
 end
